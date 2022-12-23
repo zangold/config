36f1925b6dd7db5ac4ca6d05b6bf065f63982ddf
Zach Angold <zangold@fastmail.com>
2022-12-23 02:47:28 -0500

    kitty: change font

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -184,6 +184,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/zach/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
+  },
+  ["vim-glsl"] = {
+    loaded = true,
+    path = "/home/zach/.local/share/nvim/site/pack/packer/start/vim-glsl",
+    url = "https://github.com/tikhomirov/vim-glsl"
   }
 }
 
